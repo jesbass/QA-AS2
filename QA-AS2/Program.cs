e20257d2cb9a1af4e179d5d9581115a8ed81da87
jesbass <jesbass@gmail.com>
2021-10-06 20:17:08 -0400

    Unit Tests added

--- QA-AS2/Program.cs
+++ QA-AS2/Program.cs
@@ -8,28 +8,87 @@ namespace QA_AS2
 {
     class Program
     {
+        //Shows the main menu
+        static void showMenu()
+        {
+            Console.WriteLine("1- Get length");
+            Console.WriteLine("2- Set length");
+            Console.WriteLine("3- Get width");
+            Console.WriteLine("4- Set width");
+            Console.WriteLine("5- Get area");
+            Console.WriteLine("6- Get perimeter");
+            Console.WriteLine("7- Exit");
+            Console.WriteLine("Please select an option: ");
+        }
         static void Main(string[] args)
         {
+            int opt;
             Rectangle rec = new Rectangle();
-            Console.WriteLine("Rectangle 1");
-            Console.WriteLine("Lenght: " + rec.GetLength().ToString());
-            Console.WriteLine("Width: " + rec.GetWidth().ToString());
-            Console.WriteLine("Changing lenght and width");
-            rec.SetLength(8);
-            rec.SetWidth(6);
-            Console.WriteLine("Lenght: " + rec.GetLength().ToString());
-            Console.WriteLine("Width: " + rec.GetWidth().ToString());
-            Console.WriteLine("Area: " + rec.GetArea().ToString());
-            Console.WriteLine("Perimeter: " + rec.GetPerimeter().ToString());
-            Console.WriteLine("");
-
-            Console.WriteLine("Rectangle 2");
-            Rectangle rec2 = new Rectangle(5, 4);
-            Console.WriteLine("Lenght: " + rec2.GetLength().ToString());
-            Console.WriteLine("Width: " + rec2.GetWidth().ToString());
-            Console.WriteLine("Area: " + rec2.GetArea().ToString());
-            Console.WriteLine("Perimeter: " + rec2.GetPerimeter().ToString());
-            Console.ReadLine();
+            do
+            {
+                showMenu();
+                var optString = Console.ReadLine();
+                //validate correct data type input
+                while (!int.TryParse(optString, out opt))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Wrong option. Please try again");
+                    Console.WriteLine();
+                    showMenu();
+                    optString = Console.ReadLine();
+                }
+                opt = Int32.Parse(optString);
+                if (opt > 0 && opt <= 7)
+                {
+                    switch (opt)
+                    {
+                        case 1:
+                            Console.WriteLine("Lenght: " + rec.GetLength().ToString());
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter new length: ");
+                            var lenString = Console.ReadLine();
+                            int lenint;
+                            //validate correct data type input
+                            while (!int.TryParse(lenString, out lenint))
+                            {
+                                Console.WriteLine("Wrong input, try again");
+                                lenString = Console.ReadLine();
+                            }
+                            lenint = int.Parse(lenString);
+                            rec.SetLength(lenint);
+                            break;
+                        case 3:
+                            Console.WriteLine("Width: " + rec.GetWidth().ToString());
+                            break;
+                        case 4:
+                            Console.WriteLine("Enter new width: ");
+                            var widString = Console.ReadLine();
+                            int widint;
+                            //validate correct data type input
+                            while (!int.TryParse(widString, out widint))
+                            {
+                                Console.WriteLine("Wrong input, try again");
+                                widString = Console.ReadLine();
+                            }
+                            widint = int.Parse(widString);
+                            rec.SetWidth(widint);
+                            break;
+                        case 5:
+                            Console.WriteLine("Area: " + rec.GetArea().ToString());
+                            break;
+                        case 6:
+                            Console.WriteLine("Perimeter: " + rec.GetPerimeter().ToString());
+                            break;
+                    }
+                }
+                else if (opt != 7)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Wrong option. Please try again");
+                    Console.WriteLine();
+                }
+            } while (opt != 7);
         }
     }
 }
---
+++ UnitTests/Tests.cs
@@ -0,0 +1,141 @@
+﻿
+using NUnit.Framework;
+using QA_AS2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace UnitTests
+{
+    public class Tests
+    {
+
+        [Test]
+        public void SetLength_Integer_Value()
+        {
+            var rectangle = new Rectangle();
+            var result = rectangle.SetLength(1);
+            Assert.IsInstanceOf<int>(result);
+        }
+
+        [Test]
+        public void SetWidth_Integer_Value()
+        {
+            var rectangle = new Rectangle();
+            var result = rectangle.SetWidth(1);
+            Assert.IsInstanceOf<int>(result);
+        }
+
+        [Test]
+        public void SetLength_Default()
+        {
+            var rectangle = new Rectangle();
+            int testValue = 1;
+            var result = rectangle.SetLength(testValue);
+            Assert.AreEqual(testValue, result);
+        }
+
+        [Test]
+        public void SetLength_Value()
+        {
+            var rectangle = new Rectangle();
+            var testLength = 3;
+            var result = rectangle.SetLength(testLength);
+            Assert.AreEqual(testLength, result);
+        }
+
+        [Test]
+        public void GetLength_Default()
+        {
+            var rectangle = new Rectangle();
+            var result = rectangle.GetLength();
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void GetLength_Value()
+        {
+            var rectangle = new Rectangle();
+            var result = rectangle.GetLength();
+            Assert.AreEqual(rectangle.GetLength(), result);
+        }
+
+        [Test]
+        public void SetWidth_Default()
+        {
+            var rectangle = new Rectangle();
+            int testValue = 1;
+            var result = rectangle.SetWidth(testValue);
+            Assert.AreEqual(testValue, result);
+        }
+        [Test]
+        public void SetWidth_Value()
+        {
+            var rectangle = new Rectangle();
+            var testWidth = 3;
+            var result = rectangle.SetWidth(testWidth);
+            Assert.AreEqual(testWidth, result);
+        }
+
+        [Test]
+        public void GetWidth_Value()
+        {
+            var rectangle = new Rectangle();
+            var result = rectangle.GetWidth();
+            Assert.AreEqual(rectangle.GetWidth(), result);
+        }
+
+        [Test]
+        public void GetWidth_Default()
+        {
+            var rectangle = new Rectangle();
+            var result = rectangle.GetWidth();
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void GetArea_Default()
+        {
+            var rectangle = new Rectangle();
+            var result = rectangle.GetArea();
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void GetArea_Change_Value()
+        {
+            int testLength = 3;
+            int testWidth = 5;
+            var rectangle = new Rectangle();
+            rectangle.SetLength(testLength);
+            rectangle.SetWidth(testWidth);
+            var expectedArea = testWidth * testLength;
+            var resultArea = rectangle.GetArea();
+            Assert.AreEqual(expectedArea, resultArea);
+        }
+
+        [Test]
+        public void GetPerimeter_Change_Value()
+        {
+            int testLength = 4;
+            int testWidth = 5;
+            var rectangle = new Rectangle();
+            rectangle.SetLength(testLength);
+            rectangle.SetWidth(testWidth);
+            var expectedPerimeter = (testWidth * 2) + (testLength * 2);
+            var resultPerimeter = rectangle.GetPerimeter();
+            Assert.AreEqual(expectedPerimeter, resultPerimeter);
+        }
+
+        [Test]
+        public void GetPerimeter_Default()
+        {
+            var rectangle = new Rectangle();
+            var result = rectangle.GetPerimeter();
+            Assert.AreEqual(4, result);
+        }
+    }
+}
